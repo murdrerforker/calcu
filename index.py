11da859b7666f5c7b57e55ccd11e1ddaf5fa3beb
murdrerforker <tundeadebayo129@gmail.com>
2024-03-05 16:01:44 +0100

    simple arithmetic

---
+++ index.py
@@ -0,0 +1,11 @@
+firstValue = int(input('your first value'))
+secondValue = int(input('your second value '))
+addition = firstValue + secondValue
+subtraction = firstValue - secondValue
+multiply = firstValue * secondValue
+division = firstValue / secondValue
+
+print('Addition = ',addition)
+print('Subtraction = ', subtraction)
+print('Multiply = ', multiply)
+print('Division = ' ,division)
